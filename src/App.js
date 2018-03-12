78a268cf0bdd51c510c3a377764b1717ae164892
Blaine Anderson <andersonblaine90@gmail.com>
2018-03-12 12:17:46 -0400

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import BusinessList from '../components/BusinessList/BusinessList';
-import SearchBar from '../components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
