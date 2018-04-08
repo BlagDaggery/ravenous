2b776efa7c35260100802059fe9aaabf1427d2cd
Blaine Anderson <andersonblaine90@gmail.com>
2018-04-08 18:42:44 -0400

    Update App.js and App.css to include sticky footer

--- src/App.js
+++ src/App.js
@@ -20,9 +20,16 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.state.businesses} />
+          <div className="Content">
+            <h1>ravenous</h1>
+            <SearchBar searchYelp={this.searchYelp} />
+            <BusinessList businesses={this.state.businesses} />
+          </div>
+          <footer>
+            <div className="footer-link" >
+              <a href="https://github.com/BlagDaggery/ravenous" target="_blank" title="View GitHub Repository">View GitHub Repository</a>
+            </div>
+          </footer>
       </div>
     );
   }
